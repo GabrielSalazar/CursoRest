5c77abd42191717440557353af5ff1cb5ae8b8b0
salazarg <gabriel_salazar@terceiros.sicredi.com.br>
2024-04-01 19:17:06 -0300

    Initial commit

---
+++ src/main/java/br/com/Salazar/OlaMundoTest.java
@@ -0,0 +1,41 @@
+package br.com.Salazar;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+
+public class OlaMundoTest {
+
+    @Test
+    public void testeOlaMundo(){
+        Response response = request(Method.GET,"https://restapi.wcaquino.me/ola");
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertEquals(201,response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured(){
+        Response response = request(Method.GET,"https://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+        get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+        given()
+        .when()
+            .get("https://restapi.wcaquino.me/ola")
+        .then()
+             .statusCode(200);
+    }
+    @Test
+    public void devoConhecerMatchersHamcrest(){
+        Assert.assertThat("Maria", Matchers.is("Maria"));
+    }
+}
